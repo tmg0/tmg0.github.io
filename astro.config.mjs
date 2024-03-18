ee6b3d8bfc43341d71a5c94fd7aba79752611e91
tmg0 <zekun.jin@foxmail.com>
2024-03-18 17:51:37 +0800

    build: auto output

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   site: 'https://tmg0.github.io',
   integrations: [mdx(), sitemap(), vue(), tailwind()],
 })
