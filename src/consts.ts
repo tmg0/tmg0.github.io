0ca68d223d3acbfc542eaa07c98c18e4acae85c5
tmg0 <zekun.jin@foxmail.com>
2024-03-18 15:21:22 +0800

    chore: rm unused pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'tmg0'
+export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const GITHUB_AVATAR = 'https://avatars.githubusercontent.com/u/40710111?v=4'
