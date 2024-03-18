269244e126462f386cc743e9e56bf4ad987110ab
tmg0 <zekun.jin@foxmail.com>
2024-03-18 14:56:03 +0800

    chore: use tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tmg0.github.io',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
