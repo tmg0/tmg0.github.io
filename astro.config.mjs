1b1fbdae041851bf5f1d7a93bd0914233e041ffd
tmg0 <zekun.jin@foxmail.com>
2024-03-18 11:23:03 +0800

    build: use astro github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: vercel()
+  site: 'https://tmg0.github.io',
+  integrations: [mdx(), sitemap()]
 });
